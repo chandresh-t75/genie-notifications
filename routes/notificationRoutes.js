ae5d624a55a0dee73520a78e2bb1ae5987677f4c
chandresh-t75 <2100430100016@bietjhs.ac.in>
2024-05-30 13:34:22 +0530

    inital commit

---
+++ routes/notificationRoutes.js
@@ -0,0 +1,9 @@
+// import express from 'express';
+// import { sendCustomNotificationChat } from '../controllers/notificationController.js';
+// const router = express.Router();
+
+
+// router.route('/send-notification-chat').get(sendCustomNotificationChat);
+
+
+// export default router;
